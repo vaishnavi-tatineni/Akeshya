9f49bc6be5b4cf4329708ee0bae72f28c682d19f
Vaishnavi Tatineni <tatinenivaishnavi@gmail.com>
2024-06-06 16:29:25 +0530

    deploy issue fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/edge-functions';
+import netlify from "@astrojs/netlify/functions";
 
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: netlify({
+    edgeMiddleware: true
+  })
 });
 
